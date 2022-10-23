f93ab697e9faf5a4b02f31f479b5d80fc3cf34d3
BlackH3art <jaroslaw.musielak@outlook.com>
2022-10-23 20:41:22 +0200

    feature: setting drawnNumbersArray

--- contracts/Lottery7ball.sol
+++ contracts/Lottery7ball.sol
@@ -7,6 +7,8 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 contract Lottery7ball is VRFConsumerBaseV2 {
 
 
+  uint8[] rangeArray;
+  uint8[] drawnNumbersArray;
 
   // ===================================================
   //                  CHAINLINK CONFIGURATION
@@ -14,14 +16,13 @@ contract Lottery7ball is VRFConsumerBaseV2 {
 
   VRFCoordinatorV2Interface COORDINATOR;
 
-  // polygon mumbai
   address constant vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
   bytes32 constant keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
 
   uint64 immutable subscriptionId;
   uint32 constant callbackGasLimit = 2000000;
   uint16 constant requestConfirmations = 3;
-  uint16 constant randomNumbersAmount =  6;
+  uint16 constant randomNumbersAmount =  7;
   uint256 public requestId;
 
   
@@ -32,6 +33,8 @@ contract Lottery7ball is VRFConsumerBaseV2 {
 
     COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
     subscriptionId = _subscriptionId;
+
+    rangeArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
 
   }
 
@@ -61,6 +64,35 @@ contract Lottery7ball is VRFConsumerBaseV2 {
   //               CHAINLINK FALLBACK
   // ===================================================
 
-  function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomNumbers) internal override {}
+  function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomNumbers) internal override {
+
+    addDrawnNumberToArray(randomNumbers[0] % rangeArray.length);
+    addDrawnNumberToArray(randomNumbers[1] % rangeArray.length);
+    addDrawnNumberToArray(randomNumbers[2] % rangeArray.length);
+    addDrawnNumberToArray(randomNumbers[3] % rangeArray.length);
+    addDrawnNumberToArray(randomNumbers[4] % rangeArray.length);
+    addDrawnNumberToArray(randomNumbers[5] % rangeArray.length);
+    addDrawnNumberToArray(randomNumbers[6] % rangeArray.length);
+  }
+
+
+
+
+
+
+
+  // ===================================================
+  //               UTILS / HELPERS
+  // ===================================================
+
+  function addDrawnNumberToArray(uint256 number) internal {
+    drawnNumbersArray.push(rangeArray[number]);
+
+    delete rangeArray[number];
+
+    for(uint256 i = number; i < rangeArray.length - 1; i++) {
+      rangeArray[i] = rangeArray[i + 1];
+    }
+  }
 
 }
